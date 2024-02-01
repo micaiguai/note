c4ebda4bd6b44aca2894ce8b0c56bc0fdfa67a4a
micaiguai <1399880823@qq.com>
2024-02-01 09:00:59 +0800

    feat: nprogress配置

---
+++ workspaces/sandbox_vueuse/main.js
@@ -0,0 +1,5 @@
+import { useNProgress } from '@vueuse/integrations/useNProgress'
+import nprogress from 'nprogress'
+
+console.log('useNProgress :', useNProgress)
+console.log('nprogress :', nprogress)
