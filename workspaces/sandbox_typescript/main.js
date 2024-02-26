af65083b3647d715a0921ca4feab0466ca3df5ae
micaiguai <1399880823@qq.com>
2024-02-26 10:33:35 +0800

    feat: 上传方法调整

---
+++ workspaces/sandbox_typescript/main.js
@@ -0,0 +1,10 @@
+function myForEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i);
+    }
+}
+myForEach([1, 2, 3], (a) => console.log(a));
+myForEach([1, 2, 3], (a, i) => console.log(a, i));
+myForEach([1, 2, 3], (a, i) => {
+    console.log(i.toFixed());
+});
